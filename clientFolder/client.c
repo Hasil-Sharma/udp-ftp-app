cb43000ed87d2cf5da7c824dfa63405d4081f220
Hasil Sharma <Hasil.Sharma@colorado.edu>
2017-09-24 13:21:20 -0600

    Final Implementation

--- clientFolder/client.c
+++ clientFolder/client.c
@@ -31,12 +31,12 @@
 
 #define MAXBUFSIZE 120 // Set the limit on the length of the file
 
-#define DEBUGS1(s) fprintf(stderr, "DEBUG: %s\n", s)
-#define DEBUGSX(s) fprintf(stderr, "DEBUG: %u\n", s)
-#define DEBUGN(d, s) fprintf(stderr,"DEBUG: %s: %d\n",d,s)
-#define DEBUGS(d, s) fprintf(stderr,"DEBUG: %s: %s\n",d, s)
-#define INFON(d, s) fprintf(stdout, "INFO: %s: %d\n", d, s)
-#define INFOS(d, s) fprintf(stdout, "INFO: %s: %s\n", d, s)
+#define DEBUGS1(s) // fprintf(stderr, "DEBUG: %s\n", s)
+#define DEBUGSX(s) // fprintf(stderr, "DEBUG: %u\n", s)
+#define DEBUGN(d, s) //  fprintf(stderr,"DEBUG: %s: %d\n",d,s)
+#define DEBUGS(d, s) // fprintf(stderr,"DEBUG: %s: %s\n",d, s)
+#define INFON(d, s) // fprintf(stdout, "INFO: %s: %d\n", d, s)
+#define INFOS(d, s) // fprintf(stdout, "INFO: %s: %s\n", d, s)
 
 
 struct header {
